a0ba17a04499421e2a3054dcfeb3b126ad5903c1
chanweiyan <chanweiyan007@gmail.com>
2017-06-10 18:28:10 +0800

    nested form UI for tickets

--- app/models/event.rb
+++ app/models/event.rb
@@ -30,6 +30,7 @@ class Event < ApplicationRecord
   before_validation :generate_friendly_id, :on => :create
 
   has_many :tickets, :dependent => :destroy
+  accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
 
   def to_param
     # "#{self.id}-#{self.name}"
